1f026ed938da5ba0763c6bd4f44ac58c61d5ca85
Matthias Opitz <opitz@gmx.de>
2017-11-17 12:19:27 +0000

    Build script now opens editor to update '/etc/multihost.config' and then uses the settings to deploy the 'default.configuration' file

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # script to build all docker images for multihost
-echo 'multihost builder v.1.1'
+echo 'multihost builder v.1.2'
 docker build -t centos7_php7_httpd centos7_php7_httpd
 docker build -t centos7_php56_httpd centos7_php56_httpd
 docker build -t ubuntu_php7_apache2 ubuntu_php7_apache2
@@ -17,6 +17,23 @@ sudo chmod  777 /usr/sbin/deploy_vhost
 
 sudo cp multihost.conf /etc/multihost.conf
 sudo chmod 777 /etc/multihost.conf
+
 echo '--------------------------------------------------------'
+echo "A '/etc/multihost.conf' file has been placed and needs to be configured in order to finish the inital setup."
+echo "Please press any key to open that file with an editor, make the necessay changes and save them."
+echo "The setup will continue after you have closed the editor."
+sudo nano /etc/multihost.conf
+
+# check for the config file and get the settings
+if [ ! -f /etc/multihost.conf ]
+	then
+	echo No configuration file found at /etc/multihost.conf file found - aborting!
+	exit 1
+fi
+. /etc/multihost.conf
+
+sudo cp default.configuration ${sites_enabled_path}/default.configuration
+sudo chmod 777 ${sites_enabled_path}/default.configuration
+
 echo 'All Done!'
-echo 'Please edit /etc/multihost.conf according to your needs!'
+echo "You may run 'run_multihost' to start the multihost server now."
