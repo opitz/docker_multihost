5c3d9a413ba2c651a2f42e78cbe6da7fb6a8990a
Matthias Opitz <opitz@gmx.de>
2017-11-28 17:45:40 +0000

    clean up

--- index.php
+++ index.php
@@ -34,42 +34,39 @@ if(file_exists('ip.txt')) {
 	to your local /etc/hosts/ file and connect it to the IP address of the machine this server is running on.
 </p>
 <p>
-	<p>
-		<span class="subheader">New multihost command line commands</span>
-	</p>
+	<span class="subheader">New multihost command line commands</span>
+	<br>
 	During the setup of the multihost server you installed several multihost CLI commands that need to run as superuser:
 	<ul>
 		<li><b>run_multihost</b> - (Re-)start the multihost server. By default it will run with Centos7 and PHP7.</li> 
 		<li><b>restart_multihost</b> - Restart the web server inside the Docker container to reload new config.<br>This will automatically be issued when deploying or removing VHOSTs.</li> 
 		<li><b>deploy_vhost <i>servername</i></b> - Deploy a new VHOST (see below).</li> 
-		<li><b>remove_vhost <i>servername</i></b> - Remove an existing VHOST and all it's settings - but <b>NOT</b>> the web data.</li> 
+		<li><b>remove_vhost <i>servername</i></b> - Remove an existing VHOST and all it's settings - but <b>NOT</b> the web data.</li> 
 		<li><b>multihost_default <i>servername</i></b> - Promoting one of the existing(!) VHOSTs to default web server.</li> 
 		<li><b>purge_moodlecache <i>servername</i></b> - Removing any cached moodledata for the given servername.</li> 
 	</ul>
 </p>
 <p>
-	<p>
-		<span class="subheader">Deploy new VHOST</span>
-	</p>
+	<span class="subheader">Deploy new VHOST</span>
+	<br>
 	To deploy a new VHOST do these steps in a terminal:
 	<ul>
 		<li>Add or git clone a webroot folder with the name of the new server into your basic webroot folder (e.g. /var/www/<i>servername</i>)</li>
 		<li>Issue 'sudo deploy_vhost <i>servername</i>'</li>
 		<li>Add an entry for <i>servername</i> into your local(!) /etc/hosts and link it to the IP address of this server ( <?php echo file_get_contents('ip.txt');?>).</li>
 	</ul>
-	<br>
-	<i>In the above cases replace <i>servername</i> with the actual server name you are about to deploy.</i>
 </p>
 <p>
-	<p>
-		<span class="subheader">Make a VHOST the default web content</span>
-	</p>
+	<span class="subheader">Make a VHOST the default web content</span>
+	<br>
 	To make one of the existing VHOSTs the default one simply issue
 	<ul>
 		<li>sudo multihost_default <i>servername</i></li>
 	</ul>
 
 </p>
+<br>
+In the above cases please replace <i>servername</i> with the actual server name.
 
 <hr>
 <span class="footer">v.1.3 2017-11-28</span>
