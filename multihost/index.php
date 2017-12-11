0e241d3500e47f27fd15adf4b6cf6a2cb991a68d
Matthias Opitz <opitz@gmx.de>
2017-12-11 17:54:43 +0000

    wording

--- multihost/index.php
+++ multihost/index.php
@@ -251,7 +251,7 @@ For enabled Moolde VHOSTs you can purge the Moodle cache by pressing the "Purge 
 <p>
 	<span class="subheader">New multihost command line commands</span>
 	<br>
-	During the setup of the multihost server you installed several multihost CLI commands that need to run as superuser (sudo <i>command</i>):
+	During the setup of the multihost server several multihost CLI commands were installed that need to run as superuser (sudo <i>command</i>):
 	<ul>
 		<li><b>run_multihost</b> - (Re-)start the multihost server. By default it will run with Centos7 and PHP7.</li> 
 		<li><b>restart_multihost</b> - Restart the web server inside the Docker container to reload new config.<br>This will automatically be issued when enabling or disabling VHOSTs.</li> 
