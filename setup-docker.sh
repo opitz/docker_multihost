43daf5be31642cbbfbe722c84d239d817868fe50
VASILEIOS SOTIRAS <v.sotiras@qmul.ac.uk>
2019-04-23 09:14:07 +0100

    Update setup-docker.sh
    Add docker-compose

--- setup-docker.sh
+++ setup-docker.sh
@@ -25,6 +25,11 @@ sudo groupadd docker
 
 sudo usermod -aG docker $USER
 
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 # postpone docker boot time initiations
 # echo manual | sudo tee /etc/init/docker.override
 
